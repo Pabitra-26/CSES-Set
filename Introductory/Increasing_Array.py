d84b32f60f1eb3f8bffdb1cc31bcfacb5ef70a01
Pabitra-26 <63382182+Pabitra-26@users.noreply.github.com>
2020-08-05 23:13:51 +0530

    Update Increasing_Array.py

--- Introductory/Increasing_Array.py
+++ Introductory/Increasing_Array.py
@@ -2,7 +2,7 @@
 # Description: You are given an array of n integers.
 # You want to modify the array so that it is increasing, i.e., every element is at least as large as the previous element.
 # On each turn, you may increase the value of any element by one. What is the minimum number of turns required?
-# Strategy:
+# Strategy:check from index 1 , whether it is greater than or equal to its previous element, if not, then find the difference and append it to ta list, find the sum of that list
 
 l=int(input())
 li=list(map(int,input().split()))
